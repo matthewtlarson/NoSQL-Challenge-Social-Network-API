c6bb9a417ff4489bcb4699dfdfed07afa36c1a3d
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-08-16 17:40:54 -0700

    user model

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+// https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax // match
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ]
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length});
+
+const User = model('user', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const db = require('./config/connection');
+//const routes = require('./routes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,10 +9,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+//app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
